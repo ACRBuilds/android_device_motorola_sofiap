62ac618752323640aac72ef7929d0dfdff34d08e
alexwcrafter <39865767+alexwcrafter@users.noreply.github.com>
2021-08-14 14:26:58 +0200

    Building for StellarOS

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,8 +1,8 @@
 PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/device.mk \
-    $(LOCAL_DIR)/lineage_sofiap.mk
+    $(LOCAL_DIR)/stellar_sofiap.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_sofiap-user \
-    lineage_sofiap-userdebug \
-    lineage_sofiap-eng \
+    stellar_sofiap-user \
+    stellar_sofiap-userdebug \
+    stellar_sofiap-eng \
